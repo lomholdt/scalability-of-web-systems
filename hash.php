3e79e9bf488df7d51a6d85206558e0f863356d5d
jonaslomholdt <jonaslomholdt@gmail.com>
2015-09-19 15:33:36 +0200

    updated autoloader path

---
+++ hash.php
@@ -0,0 +1,15 @@
+<?php 
+require 'vendor/autoload.php';
+$smarty = new Smarty;
+
+if(isset($_POST['input'])){
+	$input = $_POST['input'];
+	$output = [];
+	foreach (hash_algos() as $hashFunction) {
+		$output[$hashFunction] = hash($hashFunction, $input);
+	}
+	$smarty->assign('input', $input);
+	$smarty->assign('output', $output);
+}
+	$smarty->display('hash.tpl');
+?>
